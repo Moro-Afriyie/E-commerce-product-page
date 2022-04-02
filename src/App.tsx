c67670ee6feb4406df10736110c78270f5076c03
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-02 18:10:47 +0000

    setup tailwind css for the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Welcome to my react app</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
