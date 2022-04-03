ae09985c904605d80cf7fd8e43362bcbd4ec6d43
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-03 16:46:04 +0000

    add the product details

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,29 @@ function App() {
         <NavBar />
         <div className="px-16 py-10 flex justify-between">
           <Carousel />
+          <div className="product-details  border max-w-[32rem] pr-6 w-full border-red-900 flex flex-col justify-center gap-4">
+            <h3 className="uppercase text-darkOrange font-bold tracking-widest">
+              sneaker company
+            </h3>
+            <p className="capitalize text-black font-bold text-5xl mb-4">
+              Fall limited edition sneakers
+            </p>
+            <p className="text-darkGrayishBlue ">
+              These low-profile sneakers are your perfect casual wear companion.
+              Featuring a durable rubber against outer sole, they&apos;ll
+              withstand everything the weather can offer.
+            </p>
+            <div className="flex gap-3 items-center">
+              <p className="text-black font-bold text-xl">$125.00</p>
+              <div className="px-2 py-[0.1rem] bg-paleOrange rounded-md">
+                <p className="text-[0.6rem] font-bold text-darkOrange font-KumbhSans">
+                  50%
+                </p>
+              </div>
+            </div>
+            <p>$250.00</p>
+            <div className="control-buttons">buttons</div>
+          </div>
         </div>
       </div>
     </main>
