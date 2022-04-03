c70a190809996257311f3e213cfd746d09789152
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-03 00:00:53 +0000

    set up the app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,19 @@ function App() {
   const dispatch = useDispatch();
 
   return (
-    <div className="min-h-screen font-KumbhSans">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h3 className="text-gray-900 text-xl capitalize">{testValue}</h3>
-      <button
-        className="bg-red-500 px-4 py-2 border border-red-900 shadow-lg rounded-md"
-        onClick={() => dispatch(changeTestValue("welcome to my page"))}
-      >
-        change value
-      </button>
-    </div>
+    <main className="min-h-screen font-KumbhSans">
+      <div className="border border-darkOrange max-w-screen-lg mx-auto h-full w-full">
+        {/**Nav Bar */}
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h3 className="text-gray-900 text-xl capitalize">{testValue}</h3>
+        <button
+          className="bg-red-500 px-4 py-2 border border-red-900 shadow-lg rounded-md"
+          onClick={() => dispatch(changeTestValue("welcome to my page"))}
+        >
+          change value
+        </button>
+      </div>
+    </main>
   );
 }
 
