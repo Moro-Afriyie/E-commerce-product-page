2f311ee135e38a316731038654c4762e404038f2
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-04 12:14:18 +0000

    add the contents for the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,38 +6,14 @@ interface IModalProps {}
 const Modal: React.FunctionComponent<IModalProps> = (props) => {
   return (
     <div
-      id="popup-modal"
-      tabIndex={-1}
-      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full"
+      className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto  z-50 flex items-center justify-center h-full w-full"
+      id="my-modal"
     >
-      <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-        {/* Modal content */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          {/* Modal header */}
-          <div className="flex justify-end p-2">
-            <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              data-modal-toggle="popup-modal"
-            >
-              <svg
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-          {/* Modal body */}
-          <div className="p-6 pt-0 text-center">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <div className="mt-3 text-center">
+          <div className="mx-auto flex items-center justify-center h-20 w-20 rounded-full bg-green-100">
             <svg
-              className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
+              className="h-10 w-10 text-green-600"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -46,26 +22,25 @@ const Modal: React.FunctionComponent<IModalProps> = (props) => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
             </svg>
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this product?
-            </h3>
+          </div>
+          <h3 className="text-lg  font-medium text-gray-900">
+            Payment Successful!
+          </h3>
+          <div className="mt-2 px-7 py-3">
+            <p className="text-sm text-gray-500">
+              Account has been successfully registered!
+            </p>
+          </div>
+          <div className="items-center px-4 py-3">
             <button
-              data-modal-toggle="popup-modal"
-              type="button"
-              className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+              id="ok-btn"
+              className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
             >
-              Yes, Im sure
-            </button>
-            <button
-              data-modal-toggle="popup-modal"
-              type="button"
-              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-            >
-              No, cancel
+              Dismiss
             </button>
           </div>
         </div>
