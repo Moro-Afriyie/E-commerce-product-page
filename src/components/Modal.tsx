3075b9d5350b5db0e3f5796a44820a5b71a7d921
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-04 12:28:52 +0000

    add the carousel to the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Carousel from "./Carousel";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IModalProps {}
@@ -6,45 +7,10 @@ interface IModalProps {}
 const Modal: React.FunctionComponent<IModalProps> = (props) => {
   return (
     <div
-      className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto  z-50 flex items-center justify-center h-full w-full"
+      className="fixed inset-0 bg-black bg-opacity-80 overflow-y-auto  z-50 flex items-center justify-center h-full w-full"
       id="my-modal"
     >
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        <div className="mt-3 text-center">
-          <div className="mx-auto flex items-center justify-center h-20 w-20 rounded-full bg-green-100">
-            <svg
-              className="h-10 w-10 text-green-600"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 13l4 4L19 7"
-              ></path>
-            </svg>
-          </div>
-          <h3 className="text-lg  font-medium text-gray-900">
-            Payment Successful!
-          </h3>
-          <div className="mt-2 px-7 py-3">
-            <p className="text-sm text-gray-500">
-              Account has been successfully registered!
-            </p>
-          </div>
-          <div className="items-center px-4 py-3">
-            <button
-              id="ok-btn"
-              className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
-            >
-              Dismiss
-            </button>
-          </div>
-        </div>
-      </div>
+      <Carousel />
     </div>
   );
 };
