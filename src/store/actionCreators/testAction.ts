48e08b3a5631bd9ea1d05a620602c77840fb9652
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-12 17:46:48 +0000

    delete the test

--- src/store/actionCreators/testAction.ts
+++
@@ -1,8 +0,0 @@
-import { TEST_ACTION } from "../actionTypes/test";
-
-export const changeTestValue = (value: string) => {
-  return {
-    type: TEST_ACTION,
-    payload: value,
-  };
-};
--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,11 +1,9 @@
 import { modalReducer } from "./modalReducer";
 import { combineReducers } from "redux";
-import { testReducer } from "./testReducer";
 import { sidebarReducer } from "./sidebar.reducer";
 import { cartReducer } from "./cart.reducer";
 
 export const rootReducer = combineReducers({
-  test: testReducer,
   modal: modalReducer,
   sideBar: sidebarReducer,
   cart: cartReducer,
