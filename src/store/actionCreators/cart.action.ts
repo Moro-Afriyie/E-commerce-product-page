463af2130abe1cf9ed75de892f98cc30008719d4
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-12 16:16:04 +0000

    creat the logic to add an item to cart

--- src/store/actionCreators/cart.action.ts
+++ src/store/actionCreators/cart.action.ts
@@ -1,6 +1,16 @@
-export const addToCart = (product) => ({
-    return{
-        type: ADD_TO_CART,
-        payload: product
-    }
-})
+import { ADD_TO_CART } from "../actionTypes/cart.type";
+
+interface Product {
+  productid: string;
+  imageUrl: string;
+  quantity: number;
+  productName: string;
+  productPrice: number;
+}
+
+export const addToCart = (product: Product) => {
+  return {
+    type: ADD_TO_CART,
+    payload: product,
+  };
+};
