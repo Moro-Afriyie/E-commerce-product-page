6c2cbb32a5c893a92a40240668fc5b858b3a2782
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-19 17:01:28 +0000

    fix the event propagation bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ const NavBar: React.FunctionComponent = () => {
                 </p>
               </div>
             )}
-            <div className="hidden group-hover:block absolute -right-16 pt-6 top-[3.5rem]   px-2 sm:px-0 w-screen sm:w-80 xl:w-[22.5rem] sm:mr-4 md:mr-0  z-20 sm:top-4 sm:-right-[4.3rem] xl:-right-[6rem]">
+            <div className="hidden group-hover:block absolute -right-16 sm:pt-6 top-[3.5rem]   px-2 sm:px-0 w-screen sm:w-80 xl:w-[22.5rem] sm:mr-4 md:mr-0  z-20 sm:top-4 sm:-right-[4.3rem] xl:-right-[6rem]">
               <div className=" bg-white rounded-lg shadow-2xl flex flex-col w-full h-full min-h-[13.5rem]">
                 <div>
                   <p className="p-4 font-bold text-darkBlue">Cart</p>
