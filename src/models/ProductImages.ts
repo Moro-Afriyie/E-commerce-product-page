8e76e82142599e48efd6be2ded65fe51eab7e1f3
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-04-19 15:19:43 +0000

    update the images

--- src/models/ProductImages.ts
+++ src/models/ProductImages.ts
@@ -1,6 +1,6 @@
-import product1 from "../../assets/image-product-1.jpg";
-import product2 from "../../assets/image-product-2.jpg";
-import product3 from "../../assets/image-product-3.jpg";
-import product4 from "../../assets/image-product-4.jpg";
+import product1 from "../assets/image-product-1.jpg";
+import product2 from "../assets/image-product-2.jpg";
+import product3 from "../assets/image-product-3.jpg";
+import product4 from "../assets/image-product-4.jpg";
 
 export const productImages = [product1, product2, product3, product4];
